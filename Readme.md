4ee3c2b79d67e3f8174e601a1a8d09dc705a9bae
SudarshanBhatPersonal <sudarshanrbhat.srb2@gmail.com>
2021-07-25 15:39:51 +0530

    initialized a java app for devops

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# DEVOPS
+
+<P> This Repository contains the Devops process CI/CD and deployment of the jar builds using Gitlab, Jenkins, Dockers and AWS CLI.<br>
+Also automating the some of the process using Bash Scripting.</p>
+
+
+
+
+
+
+
+
+
---
+++ pipeline/devops_java_app/src/main/java/com/practise/devops_java_app/DevopsJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.practise.devops_java_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsJavaAppApplication.class, args);
+	}
+
+}
